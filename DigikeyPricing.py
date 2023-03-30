cedbfd6b9c3f85c916e404ab5ecf1e9bf08eb0d6
Kiana Goharian <gohariankiana@gmail.com>
2023-03-30 10:42:16 +0100

    digikey api module used

--- DigikeyPricing.py
+++ DigikeyPricing.py
@@ -1,21 +1,18 @@
 # modules
-import json
 import sys
-import os
-import digikey
-from digikey.v3.productinformation import KeywordSearchRequest
 from csv import reader
+
+import os
+import json
+
 from operator import itemgetter
+from requests.api import request
 import config
 
+import digikey
+from digikey.v3.productinformation import ManufacturerProductDetailsRequest
 
-# class to store the information about each component
-#   initialise variables in the class for
-#   the quantity stored as an integer
-#   the quantity available for the component stored as an integer
-#   the stock code of the component
-#   the component price stored as a float
-#   an error message if there is an error
+
 class Component:
     def __init__(self, quantity, quantity_available, stock_code, price, error_message):
         self.quantity = int(quantity)
@@ -27,19 +24,18 @@ class Component:
     # method to format the output for each component
     def comp_str(self):
         return f'''—————————————————————————————————————————————————
-Stock Code: \t\t{self.stock_code}
-Quantity Required: \t{self.quantity}
-Quantity Available: {self.quantity_available}
-Price: \t\t\t{self.price}
+Quantity Required: \t\t{self.quantity}
+Quantity Available: \t{self.quantity_available}
+Stock Code: \t\t\t{self.stock_code}
+Cut Tape Price: \t\t{self.price}
 —————————————————————————————————————————————————\n
 '''
 
     # method to format the output for each component that has an error
     def error_str(self):
         return f'''—————————————————————————————————————————————————
-Stock Code: \t\t{self.stock_code}
 Quantity required: \t{self.quantity}
-Quantity Available: \t{self.quantity_available}
+Stock Code: \t\t{self.stock_code}
 Error: \t\t\t{self.error_message}
 —————————————————————————————————————————————————\n
 '''
@@ -58,50 +54,61 @@ def view_errors(file):
 
 
 # function that gets the best price for the quantity of components required
-# taking the quantity of the parts that are required and the dictionary in which the product details are stored
 def get_price(quantity, product_dict):
-    # create null price dictionary in case there is an error with the quantity available or the stock code with
-    # values initialised to 0
-    null_price = {'break_quantity': 0, 'total_price': 0.0, 'unit_price': 0.0}
+    null_price = {'BreakQuantity': 0, 'UnitPrice': 0.0, 'TotalPrice': 0.0}
+    unsorted_price_list = []
 
-    # create an empty list that will be used to store all the prices
-    price_list = []
+    for element in product_dict['Products']:
+        for item in element['StandardPricing']:
+            unsorted_price_list.append(item)
 
-    # for each element in the standard pricing section of the dictionary where the product details are stored
-    #   append that element to the price list
-    for element in product_dict.standard_pricing:
-        price_list.append(element)
+    price_list = sorted(unsorted_price_list, key=itemgetter('BreakQuantity'))
 
-    # if the quantity is greater than 0
-    #   create a counter and initialise to 0
     if quantity > 0:
         counter = 0
-
-        # while the quantity is greater than the break quantity of the item in the price list
-        #   increment the counter by 1
-        #   if the counter becomes greater than or equal to the length of the price list
-        #       break the loop
-        # return the item in the price list at the index position of the counter - 1
-        while quantity > price_list[counter].break_quantity:
+        while quantity > price_list[counter]['BreakQuantity']:
             counter += 1
             if counter >= len(price_list):
                 break
         return price_list[counter - 1]
-
-    # if the quantity is 0
-    #   return the null price dictionary
     else:
         return null_price
 
 
+# function to return the quantity of components available
+def get_qty_available(product_dict):
+    max_num = 0
+
+    for i in product_dict['Products']:
+        max_num = max(max_num, i['QuantityAvailable'])
+
+    return max_num
+
+
+# # function to return the digikey part_dict number for the component
+# def get_dk_part_number(product_dict):
+#     return product_dict['Products'][0]['DigiKeyPartNumber']
+
+
+def find_keys(node, kv):
+    if isinstance(node, list):
+        for i in node:
+            for x in find_keys(i, kv):
+               yield x
+    elif isinstance(node, dict):
+        if kv in node:
+            yield node[kv]
+        for j in node.values():
+            for x in find_keys(j, kv):
+                yield x
+
+
+def my_function(x):
+    return list(dict.fromkeys(x))
+
+
 # main program
 if __name__ == '__main__':
-
-    # create two empty lists
-    # one will store the component objects that do not show errors
-    # the other will store the component objects that do show errors
-    component_list = []
-    error_list = []
 
     # define the environment variables from the digikey docs
     # https://pypi.org/project/digikey-api/
@@ -110,62 +117,116 @@ if __name__ == '__main__':
     # can be changed to false later to use as a production app
     os.environ['DIGIKEY_CLIENT_ID'] = config.client_id
     os.environ['DIGIKEY_CLIENT_SECRET'] = config.client_secret
-    os.environ['DIGIKEY_CLIENT_SANDBOX'] = 'True'
+    os.environ['DIGIKEY_CLIENT_SANDBOX'] = 'False'
     os.environ['DIGIKEY_STORAGE_PATH'] = config.cache_dir
+
+    # call the digikey product details method with a valid stock code
+    # NOTE: The purpose for this call is to bypass oauth2
+    part = digikey.product_details('TMK105BJ104KV-F')
+
+    # create two empty lists
+    # one will store the component objects that do not show errors
+    # the other will store the component objects that do show errors
+    component_list = []
+    error_list = []
 
     # open file to read into from the first argument in the command line
     # in this case this is the bill of materials csv file
     # store it as a list excluding the first line of the file
     # close the file
-    f = open(sys.argv[1], "r")
+    f = open('Bill Of Materials PowerPortMax-v5.csv', "r")
     bom = f.readlines()[1:]
     f.close()
-    # f = open('Bill Of Materials PowerPortMax-v5.csv', "r")
-    # bom = f.readlines()[1:]
-    # bom = reader(bom)
-    # f.close()
 
     # store the number of sets of parts wanted to check the pricing for which will be given as the second
     # command line argument
-    num_parts = int(sys.argv[2])
+    num_parts = 50
+
+    # digikey api endpoint to use to obtain product information
+    host = 'https://api.digikey.com/PackageTypeByQuantity/v3/Products/'
+
+    f = open('token_storage.json', 'r')
+    token_file = f.read()
+    token_dict = json.loads(token_file)
+    f.close()
+
+    # initiate headers in line with digikey documentation configured for UK use
+    HEADERS = {
+        'X-DIGIKEY-Client-Id': config.client_id,
+        'Authorization': 'Bearer ' + token_dict['access_token'],
+        'X-DIGIKEY-Locale-Site': 'UK',
+        'X-DIGIKEY-Locale-Language': 'en',
+        'X-DIGIKEY-Locale-Currency': 'GBP',
+        'X-DIGIKEY-Locale-ShipToCountry': 'uk',
+        'X-DIGIKEY-Customer-Id': '0'
+    }
 
     # for each line of the bill of materials file
     #   find the quantity of the component in the file and multiply by the number of sets of parts required specified as
     #   the second argument in the command line and store it
     #   find the stock code for the component in the file and store it
-    #   store these in a dictionary
     #   use the quantity and stock code to query the api
     #   store the response to the query including the required variables
     for line in reader(bom):
-        quantity = int(line[1]) * int(num_parts)
-        stock_code = line[4]
-        part_dict = digikey.product_details(stock_code)
+        component_info = line
+        quantity = int(component_info[1]) * int(num_parts)
+        stock_code = component_info[4]
 
-        # if the quantity of the part_dict that is available is less that the quantity that is required
-        #   create the relevant error message
-        #   store that component as an object using the Component class
-        #   make sure the price shows there is an error and store the error message in the object
-        #   append object to the error list
-        if part_dict.quantity_available < quantity:
-            error_message = 'Quantity required not available'
-            component = Component(quantity, part_dict.quantity_available, stock_code, "Error", error_message)
-            error_list.append(component)
+        # Search for parts
+        search_request = ManufacturerProductDetailsRequest(stock_code, record_count=10)
+        result = digikey.manufacturer_product_details(body=search_request)
 
-        # if the quantity available is not less that the quantity of the part_dict that is required
-        #   find the total price for the quantity that is required
-        #   create a component object using the component class created earlier with the relevant attributes
-        #   append this to the component list
+        result_dict = result.to_dict()
+
+        dkpn_list = list(find_keys(result_dict, 'digi_key_part_number'))
+
+        if len(dkpn_list) > 0:
+            dk_part_nums = my_function(dkpn_list)
+
+            host_URL = host + dk_part_nums[0]
+            PARAMS = {
+                'RequestedQuantity': str(quantity),
+                'Includes': 'Products(DigiKeyPartNumber,QuantityAvailable,StandardPricing)'
+            }
+            response = request(method="GET", url=host_URL, headers=HEADERS, params=PARAMS)
+
+            # if the response is successful
+            #   the error message is empty
+            #   store the contents of the json file created as a dictionary
+            #   use the dictionary to extract the applicable best price for the quantity required
+            #   extract the digikey part_dict number for the component
+            #   extract the unit price for the component
+            #   extract the appropriate total price for the component
+            #   store this information as a component object using Component class
+            #   append this object to the component list
+            if response.status_code == 200:
+                error_message = ""
+                product_dict = json.loads(str(response.content)[2:-1])
+                quantity_in_stock = get_qty_available(product_dict)
+                # DigiKeyPartNumber = get_dk_part_number(product_dict)
+                unit_price = get_price(quantity, product_dict)['UnitPrice']
+                total_price = get_price(quantity, product_dict)['UnitPrice'] * quantity
+                component = Component(quantity, quantity_in_stock, stock_code, round(total_price, 2), "")
+                component_list.append(component)
+
+            # if there is an error
+            #   store the error message from the response
+            #   store this information as a component object using Component class
+            #   set the quantity available parameter and the price parameter to 0
+            #   append to the error list
+            else:
+                error_message = f"Error: {response.text}"
+                component = Component(quantity, 0, stock_code, 0, error_message)
+                error_list.append(component)
         else:
-            total_price = get_price(quantity, part_dict).unit_price * quantity
-            component = Component(quantity, part_dict.quantity_available, stock_code, round(total_price, 2), "")
-            component_list.append(component)
-
-    # NOTE: further check needed to validate the stock codes and check whether they exist
+            error_message = 'Error: This Manufacturer Code cannot be found'
+            component = Component(quantity, 0, stock_code, 0, error_message)
+            error_list.append(component)
 
     # open a file to write the components from both lists into
     # to show the prices for each component and instances where an error has occured
     # close the file
-    f = open("DKPricingTest.txt", "w")
+    f = open("DigikeyPricing_1.txt", "w")
     f.write("Available Components:\n")
     view_components(f)
     f.write("\nData That Does Not Match:\n")
@@ -174,7 +235,7 @@ if __name__ == '__main__':
 
     # open the same file to read into and store the contents of the file
     # then print the file to the console
-    f = open("DKPricingTest.txt", "r")
+    f = open("DigikeyPricing_1.txt", "r")
     dk_pricing = f.read()
     f.close()
     print(dk_pricing)
